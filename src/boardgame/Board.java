df986e47f542517bb4b7591cd709d49c70a90ee7
Martin Schmidt <mmartin.schmidt@gmail.com>
2020-05-30 17:46:43 -0300

    Piece move count

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -49,7 +49,7 @@ public class Board {
 		piece.position = position;
 	}
 	
-	public Piece RemovePiece(Position position) {
+	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
 			//throw new BoardException("Position not on the board");
 			throw new BoardException("Posicao fora do tabuleiro");
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,8 +86,9 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.RemovePiece(source);
-		Piece capturedPiece = board.RemovePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
+		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
 		if (capturedPiece != null) {
@@ -99,7 +100,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.RemovePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if (capturedPiece != null) {
